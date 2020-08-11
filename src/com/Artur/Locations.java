27445d28d25629df2b497b4fa2ed720639dd3263
PrIvATe <kpackalv@yahoo.com>
2020-08-11 22:11:33 +0300

    DataOutputStream & BufferedOutputStream & FileOutputStream

--- src/com/Artur/Locations.java
+++ src/com/Artur/Locations.java
@@ -7,33 +7,24 @@ public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new LinkedHashMap<>();
 
     public static void main(String[] args) throws IOException {
-        //try with resources - automatically ensures that the file writer stream is closed whether  code executes normally or with exception occurs.
+        try (DataOutputStream locFile = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("locations.dat")));){
+            for (Location location : locations.values()) {
+                locFile.writeInt(location.getLocationID());
+                locFile.writeUTF(location.getDescription()); //to write a String
 
-        try(BufferedWriter locFile = new BufferedWriter(new FileWriter("locations.txt"));
-            BufferedWriter dirFile = new BufferedWriter (new FileWriter("directions.txt"))){
-            for(Location location : locations.values()){
-                locFile.write(location.getLocationID() +","+ location.getDescription());
-                locFile.newLine();
-                for(String direction : location.getExits().keySet()){
-                    if(!direction.equalsIgnoreCase("Q")) {
-                        dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction));
-                        dirFile.newLine();
+                System.out.println("Writing location " + location.getDescription() + " : " + location.getDescription());
+                System.out.println("Writing " + (location.getExits().size() - 1) + " exits.");
+
+                locFile.writeInt(location.getExits().size() - 1);
+                for (String direction : location.getExits().keySet()) {
+                    if (!direction.equalsIgnoreCase("Q")) {
+                        System.out.println("\t\t" + direction + "," + location.getExits().get(direction));
+                        locFile.writeUTF(direction);
+                        locFile.writeInt(location.getExits().get(direction));
                     }
                 }
             }
         }
-
-//        try (FileWriter locFile = new FileWriter("locations.txt");
-//             FileWriter dirFile = new FileWriter("directions.txt")) {
-//            for (Location location : locations.values()) {
-//                locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
-//                for (String direction : location.getExits().keySet()) {
-//                    dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction) + "\n");
-//                }
-//            }
-//        }
-
-
     }
 
     static {
